25d895a1b62d33b5290bfbcbbcb431154ec1ccfa
02kK <lahass1213@gmail.com>
2022-05-24 08:36:48 +0900

    object

--- src/chap3/obj.ts
+++ src/chap3/obj.ts
@@ -17,3 +17,23 @@ const obj: objType = {
 const bar: number = obj.bar;
 const one = obj["1"];
 const baz = obj.baz;
+
+type objType2 = {
+  foo: string;
+  bar?: number;
+};
+
+const obj2_1: objType2 = {
+  foo: "",
+};
+
+const obj2_2: objType2 = {
+  foo: "",
+  bar: 1,
+};
+obj2_1.bar;
+if (obj2_2.bar !== undefined) {
+  obj2_2.bar / 100;
+}
+
+type copyType = typeof obj2_2
