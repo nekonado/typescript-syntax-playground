b211f25def83d02ca3e20485dd69b1c4578233ab
queuek <lahass1213@gmail.com>
2022-12-11 20:15:43 +0900

    test: non_local_exits

--- src/chap5/non_local_exits.ts
+++ src/chap5/non_local_exits.ts
@@ -1,7 +1,7 @@
 try {
-  console.log("start try 1");
+  console.log("start try 1-1");
   try {
-    console.log("start try 2");
+    console.log("start try 2-1");
     throw new Error("2層目で例外が発生しました");
     console.log("no run");
   } catch (err) {
@@ -9,6 +9,20 @@ try {
   }
   // 例外処理は2層目で完了したので、1層目では例外は隠蔽されている
   console.log("run");
+} catch (err) {
+  console.log(err);
+}
+
+console.log("-------------------");
+
+try {
+  console.log("start try 2-1");
+  try {
+    console.log("start try 2-2");
+    throw new Error("大域脱出のテスト");
+  } finally {
+    console.log("finallyブロックは脱出に割り込むことができます");
+  }
 } catch (err) {
   console.log(err);
 }
